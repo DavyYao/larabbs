de8190829b321db4b4fc9b2830b8269f04f8607d
Davv Yao <ym521105@gmail.com>
2024-09-16 15:21:14 +0800

    裁剪头像

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://152.32.175.167'),
+    'url' => env('APP_URL', 'http://www.chiningmeng.com'),
 
     'asset_url' => env('ASSET_URL'),
 
