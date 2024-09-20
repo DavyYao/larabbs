dac66b03232cac7b253ee387b3cfb8cf9f13e74f
Davv Yao <ym521105@gmail.com>
2024-09-20 18:38:57 +0800

    新增 generator 扩展

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -3,7 +3,11 @@
 
 @section('content')
     <?php
-        echo date('Y-m-d H:i:s',1725849907).'<br />';
+        echo date('Y-m-d H:i:s',1726818028).'<br />';
+        echo date('Y-m-d H:i:s',1726800540).'<br />';
+        echo strtotime('2024-09-19 07:40:01').'<br />';
+        echo strtotime('2024-09-19 08:36:39').'<br />';
+        echo hash('sha256','2ed20b76-80c0-18cf-9760-84563e5849ef').'<br />';
         echo mb_substr('粤A1234OO', -2, 2)
     ?>
   <h1>这里是首页ssss</h1>
