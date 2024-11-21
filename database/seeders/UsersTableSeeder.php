04494a08b824453230265cd37a6a6debee74b30c
DavyYao <ym521105@gmail.com>
2024-11-21 15:12:44 +0800

    集成 laravel-permission 扩展包

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,9 +20,19 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Davv Yao';
+        $user->name = 'DavvYao';
         $user->email = '313984045@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+        $user->save();
+        
+        $user = User::find(2);
+        $user->name = 'Kk石头';
+        $user->email = '939705238@qq.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        // 将 2 号用户指派为『管理员』
+        $user->assignRole('Maintainer');
         $user->save();
     }
 }
